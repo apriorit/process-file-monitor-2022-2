787ec1b8fd36c139daf7ffa712b76c9c66a4d24d
Itam <matipietka@gmail.com>
2022-09-21 00:53:40 +0200

    Tutorial passed

--- src/UI/main.cpp
+++ src/UI/main.cpp
@@ -1,11 +1,18 @@
 #include "mainwindow.h"
-
+#include "mymodel.h"
+#include <QTableView>
+#include <QTreeView>
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+
+    //MainWindow w;
+    QTreeView tableView;
+    MyModel model;
+    tableView.setModel(&model);
+    tableView.show();
+    //w.show();
     return a.exec();
 }
