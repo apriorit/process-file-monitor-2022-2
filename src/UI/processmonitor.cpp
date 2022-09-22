bf2e8ff4e53c49ce51ae860899247605f0c5ce54
Itam <matipietka@gmail.com>
2022-09-22 02:21:34 +0200

    Added definitions for process monitor

--- src/UI/processmonitor.cpp
+++ src/UI/processmonitor.cpp
@@ -3,3 +3,30 @@
 bool operator<(const ProcessInfo& first , const ProcessInfo& second){
     return first.Pid < second.Pid;
 }
+
+void ProcessMonitor::updateProcessesTable() {
+
+}
+
+ProcessInfo ProcessMonitor::getCopyOfProcessInfoByIndex(const int index){
+    return ProcessInfo(index,"test");
+}
+
+ProcessInfo ProcessMonitor::getCopyOfProcessInfoByPid(const int index){
+    return ProcessInfo(index,"test");
+}
+
+std::set<ProcessInfo> ProcessMonitor::mergeProcessesSets(const std::set<ProcessInfo>& oldSet ,
+                                                                const std::set<ProcessInfo>& newSet){
+    return std::set<ProcessInfo>();
+}
+
+void ProcessMonitor::setProcessEditableField(const ProcessEditableFields field){
+
+}
+
+ProcessMonitor::~ProcessMonitor(){
+
+}
+
+
--- src/UI/processmonitor.h
+++ src/UI/processmonitor.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <QString>
+#include <set>
 
 struct ProcessInfo{
     ProcessInfo(int pid,const QString& name)
@@ -13,20 +14,46 @@ struct ProcessInfo{
     bool closePermission = true;
     bool deletePermission = true;
     bool isMonitored = false;
-    bool isInjected = false;
+    bool isDllInjected = false;
 
     friend bool operator<(const ProcessInfo& first , const ProcessInfo& second);
 };
 
-
+enum class ProcessEditableFields{
+    readPerm,
+    writePerm,
+    openPerm,
+    closePerm,
+    deletePerm,
+    isMonitored,
+    isDllInjected
+};
 
 class IProcessMonitor{
 public:
     virtual void updateProcessesTable() = 0;
-    virtual const ProcessInfo& getProcessInfo(const int index) = 0;
-    virtual ~IProcessMonitor();
+    virtual ProcessInfo getCopyOfProcessInfoByIndex(const int Index) = 0;
+    virtual ProcessInfo getCopyOfProcessInfoByPid(const int Pid) = 0;
+    virtual void setProcessEditableField(const ProcessEditableFields field) = 0;
+    virtual ~IProcessMonitor(){}
+};
+
+class IGetProcessesInfo{
+    virtual std::set<ProcessInfo> getProcessesInfo();
 };
 
 class ProcessMonitor : public IProcessMonitor{
-
+public:
+    ProcessMonitor(IGetProcessesInfo* getProcessesInfo)
+        :getProcessesInfo{getProcessesInfo}{};
+    void updateProcessesTable() override;
+    ProcessInfo getCopyOfProcessInfoByIndex(const int index) override;
+    ProcessInfo getCopyOfProcessInfoByPid(const int Pid) override;
+    static std::set<ProcessInfo> mergeProcessesSets(const std::set<ProcessInfo>& oldSet ,
+                                                    const std::set<ProcessInfo>& newSet);
+    void setProcessEditableField(const ProcessEditableFields field) override;
+    ~ProcessMonitor();
+private:
+    std::set<ProcessInfo> processesInfo;
+    IGetProcessesInfo* getProcessesInfo;
 };
