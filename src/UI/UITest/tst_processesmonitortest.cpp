e19c5ac2f6959988c3b3d5b2c250f04e536c2eee
Itam <matipietka@gmail.com>
2022-09-23 02:02:06 +0200

    Added mergeProcesses implemantation

--- src/UI/UITest/tst_processesmonitortest.cpp
+++ src/UI/UITest/tst_processesmonitortest.cpp
@@ -2,6 +2,7 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock-matchers.h>
 #include <gmock/gmock.h>
+#include <iostream>
 #include "../processmonitor.h"
 #include "../processesseeker.h"
 
@@ -20,7 +21,7 @@ TEST(ProcessMonitorTest, processesSeekerWillBeCalledOnce){
             .WillOnce(Return(std::vector<ProcessInfo>()));
 
     sut.updateProcessesTable();
-};
+}
 
 TEST(ProcessMonitorTest, mergeProcessesBothEquals){
     std::vector<ProcessInfo> processes({{1,"p1"},{2,"p2"}});
@@ -28,6 +29,52 @@ TEST(ProcessMonitorTest, mergeProcessesBothEquals){
     std::vector<ProcessInfo> expected({{1,"p1"},{2,"p2"}});
     std::vector<ProcessInfo> result = ProcessMonitor::mergeProcessesLists(processes, update);
 
-    EXPECT_EQ(result == expected,true);
-};
+    EXPECT_EQ(result == expected , true);
+}
+
+TEST(ProcessMonitorTest, mergeProcessesBothEqualsSaveOldSettings){
+    ProcessInfo processInfo(10,"C:\\Berserk.exe");
+    processInfo.closePermission = false;
+    processInfo.deletePermission = false;
+    processInfo.readPermission = false;
+    processInfo.isDllInjected = true;
+
+    std::vector<ProcessInfo> processes({processInfo,{2,"p2"}});
+    std::vector<ProcessInfo> update({{10,"C:\\Berserk.exe"},{2,"p2"}});
+
+    std::vector<ProcessInfo> expected({processInfo,{2,"p2"}});
+    std::vector<ProcessInfo> result = ProcessMonitor::mergeProcessesLists(processes, update);
+
+    EXPECT_EQ(result, expected);
+}
+
+TEST(ProcessMonitorTest, mergeProcessesNewProcessesBorned){
+    std::vector<ProcessInfo> processes({{1,"p1"},{2,"p2"}});
+    std::vector<ProcessInfo> update({{1,"p1"},{2,"p2"},{3,"p3"},{4,"p4"}});
+
+    std::vector<ProcessInfo> expected({{1,"p1"},{2,"p2"},{3,"p3"},{4,"p4"}});
+    std::vector<ProcessInfo> result = ProcessMonitor::mergeProcessesLists(processes, update);
+
+    EXPECT_EQ(result, expected);
+}
+
+TEST(ProcessMonitorTest, mergeProcessesNewProcessUnderTheSamePid){
+    std::vector<ProcessInfo> processes({{1,"p1"},{2,"p2"}});
+    std::vector<ProcessInfo> update({{1,"p1"},{2,"p22"}});
+
+    std::vector<ProcessInfo> expected({{1,"p1"},{2,"p22"}});
+    std::vector<ProcessInfo> result = ProcessMonitor::mergeProcessesLists(processes, update);
+
+    EXPECT_EQ(result, expected);
+}
+
+TEST(ProcessMonitorTest, mergeProcessesOldProcessesTerminated){
+    std::vector<ProcessInfo> processes({{1,"p1"},{2,"p2"},{3,"p3"},{4,"p4"}});
+    std::vector<ProcessInfo> update({{1,"p1"},{2,"p2"}});
+
+    std::vector<ProcessInfo> expected({{1,"p1"},{2,"p2"}});
+    std::vector<ProcessInfo> result = ProcessMonitor::mergeProcessesLists(processes, update);
+
+    EXPECT_EQ(result, expected);
+}
 
--- src/UI/processmonitor.cpp
+++ src/UI/processmonitor.cpp
@@ -18,7 +18,16 @@ bool operator<(const ProcessInfo& first , const ProcessInfo& second){
 }
 
 bool operator==(const ProcessInfo& first , const ProcessInfo& second){
-    return first.Pid == second.Pid && first.Path == second.Path;
+    return first.Pid == second.Pid
+            && first.Path == second.Path
+            && first.Name == second.Name
+            && first.closePermission == second.closePermission
+            && first.deletePermission == second.deletePermission
+            && first.openPermission == second.openPermission
+            && first.readPermission == second.readPermission
+            && first.writePermission == second.writePermission
+            && first.isDllInjected == second.isDllInjected
+            && first.isMonitored == second.isMonitored;
 }
 
 void ProcessMonitor::updateProcessesTable() {
@@ -29,13 +38,38 @@ ProcessInfo ProcessMonitor::getCopyOfProcessInfoByIndex(const int index){
     return ProcessInfo(index,"test");
 }
 
-ProcessInfo ProcessMonitor::getCopyOfProcessInfoByPid(const int index){
-    return ProcessInfo(index,"test");
+ProcessInfo ProcessMonitor::getCopyOfProcessInfoByPid(const int Pid){
+    return ProcessInfo(Pid,"test");
 }
 
 std::vector<ProcessInfo> ProcessMonitor::mergeProcessesLists(const std::vector<ProcessInfo>& oldProcesses ,
                                                                 const std::vector<ProcessInfo>& currentProcesses){
-    return currentProcesses;
+    std::vector<ProcessInfo> updatedProcesses;
+    auto itOld = oldProcesses.begin();
+    auto itCurrent = currentProcesses.begin();
+
+    while(itOld != oldProcesses.end() && itCurrent != currentProcesses.end()){
+        if (itOld->Pid > itCurrent->Pid
+                || (itOld->Pid == itCurrent->Pid && itOld->Path != itCurrent->Path)){
+            updatedProcesses.push_back(*itCurrent);
+            itCurrent++;
+        }
+        else if(itOld->Pid == itCurrent->Pid){
+            updatedProcesses.push_back(*itOld);
+            itOld++;
+            itCurrent++;
+        }
+        else{
+            itOld++;
+        }
+    }
+
+    while(itCurrent != currentProcesses.end()){
+        updatedProcesses.push_back(*itCurrent);
+        itCurrent++;
+    }
+
+    return updatedProcesses;
 }
 
 void ProcessMonitor::setProcessEditableField(const ProcessEditableFields field){
