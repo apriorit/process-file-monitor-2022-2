c3bda47bf88949b235054bcf690ac91ed1718942
Muratcan Akcay <muratcanakcay@gmail.com>
2022-09-26 07:37:19 +0200

    add return value checks to functions

--- src/Injector/inject.cpp
+++ src/Injector/inject.cpp
@@ -2,30 +2,72 @@
 
 int inject(DWORD PID, LPCSTR DllPath)
 {
+    DWORD err;
+    int ret;
+
     // Open a handle to target process
     HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, PID);
+    if (hProcess == NULL)
+    {
+        err =  GetLastError();
+        throw std::runtime_error("Couldn't open target process. Error code:" + std::to_string(err) + "\n");
+    }
 
     // Allocate memory for the dllpath in the target process
     // length of the path string + null terminator
     LPVOID pDllPath = VirtualAllocEx(hProcess, 0, strlen(DllPath) + 1, MEM_COMMIT, PAGE_READWRITE);
+    if (pDllPath  == NULL)
+    {
+        err =  GetLastError();
+        throw std::runtime_error("Couldn't allocate memory in target process. Error code:" + std::to_string(err) + "\n");
+    }
 
     // Write the path to the address of the memory just allocated
     // in the target process
-    WriteProcessMemory(hProcess, pDllPath, (LPVOID)DllPath, strlen(DllPath) + 1, 0);
+    ret = WriteProcessMemory(hProcess, pDllPath, (LPVOID)DllPath, strlen(DllPath) + 1, 0);
+    if (ret == 0)
+    {
+        err =  GetLastError();
+        throw std::runtime_error("Couldn't write to memory in target process. Error code:" + std::to_string(err) + "\n");
+    }
 
     // Create a remote thread in the target process which calls LoadLibraryA with
     // DllPath as argument -> program loads the dll
-    HANDLE hLoadThread = CreateRemoteThread(hProcess, 0, 0,
-            (LPTHREAD_START_ROUTINE)GetProcAddress(GetModuleHandleA("Kernel32.dll"), "LoadLibraryA"), pDllPath, 0 ,0);
+    HMODULE hKernel32 = GetModuleHandleA("Kernel32.dll");
+    if (hKernel32 == NULL)
+    {
+        err =  GetLastError();
+        throw std::runtime_error("Couldn't get Kernel32.dll module handle. Error code:" + std::to_string(err) + "\n");
+    }
+
+    FARPROC kernel32Address = GetProcAddress(hKernel32, "LoadLibraryA");
+    if (kernel32Address == NULL)
+    {
+        err =  GetLastError();
+        throw std::runtime_error("Couldn't get Kernel32.dll address. Error code:" + std::to_string(err) + "\n");
+    }
+
+    HANDLE hLoadThread = CreateRemoteThread(hProcess, 0, 0, (LPTHREAD_START_ROUTINE)kernel32Address, pDllPath, 0, 0);
 
     // Wait for the execution of the loader thread to complete
     WaitForSingleObject(hLoadThread, INFINITE);
+
+    if (hLoadThread == NULL)
+    {
+        err =  GetLastError();
+        throw std::runtime_error("Couldn't create remote thread. Error code:" + std::to_string(err) + "\n");
+    }
 
     std::cout << "Dll path allocated at: " << pDllPath << std::endl;
     std::cin.get();
 
     // Free the memory allocated for the dll path
-    VirtualFreeEx(hProcess, pDllPath, strlen(DllPath) + 1, MEM_RELEASE);
+    ret = VirtualFreeEx(hProcess, pDllPath, strlen(DllPath) + 1, MEM_RELEASE);
+    if (ret == 0)
+    {
+        err =  GetLastError();
+        throw std::runtime_error("Couldn't free allocated memory in target process. Error code:" + std::to_string(err) + "\n");
+    }
 
     return 0;
 }
--- src/Injector/main.cpp
+++ src/Injector/main.cpp
@@ -1,13 +1,22 @@
 #include "inject.cpp"
 
 int main()
-{    
-    // path to dll
-    LPCSTR DllPath = "C:\\CodeBase\\Apriorit-Project\\build-src-Desktop_Qt_6_3_1_MinGW_64_bit-Debug\\Hooks\\debug\\Hooks.dll";
-    // target process PID
-    DWORD PID = 20344;
+{
+    try
+    {
+        //  path to dll
+        LPCSTR DllPath = "C:\\CodeBase\\Apriorit-Project\\build-src-Desktop_Qt_6_3_1_MinGW_64_bit-Debug\\Hooks\\debug\\Hooks.dll";
+        // target process PID
+        DWORD PID = 26260;
 
-    inject(PID, DllPath);
+        inject(PID, DllPath);
 
-    return 0;
+        return 0;
+    }
+    catch (const std::exception& e)
+    {
+        std::cout << std::endl << "Exception occurred during execution: " << e.what();
+
+        return -1;
+    }
 }
